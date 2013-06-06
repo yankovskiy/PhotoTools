8523c952446ecd86bc943be54334666c5a190f7c
Artem Yankovskiy <artemyankovskiy@gmail.com>
2013-06-06 18:07:05 +1100

    DoF activity designed
    DoF activity designed.
    Clickable links in About activity.
    Renaming resources for single code style.

--- src/ru/neverdark/phototools/AboutActivity.java
+++ src/ru/neverdark/phototools/AboutActivity.java
@@ -1,6 +1,9 @@
 package ru.neverdark.phototools;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 import android.app.Activity;
 
 public class AboutActivity extends Activity {
@@ -9,5 +12,14 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
+		
+		/* For clickable links */
+		TextView labelAuthor = (TextView) findViewById(R.id.about_label_author);
+		labelAuthor.setText(Html.fromHtml(getString(R.string.about_label_author)));
+		labelAuthor.setMovementMethod(LinkMovementMethod.getInstance());
+		
+		TextView labelLicense = (TextView) findViewById(R.id.about_label_license);
+		labelLicense.setText(Html.fromHtml(getString(R.string.about_label_license)));
+		labelLicense.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 }
--- src/ru/neverdark/phototools/MainActivity.java
+++ src/ru/neverdark/phototools/MainActivity.java
@@ -7,30 +7,30 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    public void onClick(View v) {    	    	
-    	Intent intent;
-    	switch(v.getId()) {
-    	case R.id.button_about:
-    		intent = new Intent(this, AboutActivity.class);
-    		startActivity(intent);
-    		break;
-    	case R.id.button_dofcalc:
-    		intent = new Intent(this, DofActivity.class);
-    		startActivity(intent);
-    		break;
-    	case R.id.button_evpairs:
-    		intent = new Intent(this, EvpairsActivity.class);
-    		startActivity(intent);
-    		break;
-    	case R.id.button_settings:
-    		intent = new Intent(this, SettingsActivity.class);
-    		startActivity(intent);
-    	}    	    	
-    }   
+	public void onClick(View v) {
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.main_button_about:
+			intent = new Intent(this, AboutActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.main_button_dofcalc:
+			intent = new Intent(this, DofActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.main_button_evpairs:
+			intent = new Intent(this, EvpairsActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.main_button_settings:
+			intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+		}
+	}
 }
