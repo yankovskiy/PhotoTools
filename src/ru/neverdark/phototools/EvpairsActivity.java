48f1d0aff7eddcd93d8e5c3ac4a2652659c7ffda
Artem Yankovskiy <artemyankovskiy@gmail.com>
2013-06-09 22:13:03 +1100

    Error handler for EvpairsActivity

--- src/ru/neverdark/phototools/EvpairsActivity.java
+++ src/ru/neverdark/phototools/EvpairsActivity.java
@@ -1,27 +1,44 @@
 package ru.neverdark.phototools;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 import android.app.Activity;
 
 
 public class EvpairsActivity extends Activity {
 
+	private Spinner mSpinner_currentAperture;
+	private Spinner mSpinner_currentIso;
+	private Spinner mSpinner_currentShutterSpeed;
+	private Spinner mSpinner_newAperture;
+	private Spinner mSpinner_newIso;
+	private Spinner mSpinner_newShutterSpeed;
+	
+	private int mCurrentAperturePosition;
+	private int mCurrentIsoPosition;
+	private int mCurrentShutterSpeedPosition;
+	private int mNewAperturePosition;
+	private int mNewIsoPostion;
+	private int mNewShutterSpeedPosition;
+	
+	private static final byte MAXIMUM_ALLOWED_EMPTY_FIELDS = 2;	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_evpairs);
-				
-
+		setContentView(R.layout.activity_evpairs);		
+		
 		/* Create link between resource and object */
-		Spinner spinner_currentAperture = (Spinner) findViewById(R.id.evpairs_spinner_currentAperture);			
-		Spinner spinner_currentIso = (Spinner) findViewById(R.id.evpairs_spinner_currentIso);
-		Spinner spinner_currentShutterSpeed = (Spinner) findViewById(R.id.evpairs_spinner_currentShutterSpeed);
+		mSpinner_currentAperture = (Spinner) findViewById(R.id.evpairs_spinner_currentAperture);			
+		mSpinner_currentIso = (Spinner) findViewById(R.id.evpairs_spinner_currentIso);
+		mSpinner_currentShutterSpeed = (Spinner) findViewById(R.id.evpairs_spinner_currentShutterSpeed);
 
-		Spinner spinner_newAperture = (Spinner) findViewById(R.id.evpairs_spinner_newAperture);
-		Spinner spinner_newIso = (Spinner) findViewById(R.id.evpairs_spinner_newIso);
-		Spinner spinner_newShutterSpeed = (Spinner) findViewById(R.id.evpairs_spinner_newShutterSpeed);
+		mSpinner_newAperture = (Spinner) findViewById(R.id.evpairs_spinner_newAperture);
+		mSpinner_newIso = (Spinner) findViewById(R.id.evpairs_spinner_newIso);
+		mSpinner_newShutterSpeed = (Spinner) findViewById(R.id.evpairs_spinner_newShutterSpeed);			
 		
 		
 		/* Load data from arrays */			
@@ -29,36 +46,149 @@ public class EvpairsActivity extends Activity {
 				this, android.R.layout.simple_spinner_item,
 				EvpairsCalculator.APERTURE_LIST);
 		adapter_currentAperture.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_currentAperture.setAdapter(adapter_currentAperture);
+		mSpinner_currentAperture.setAdapter(adapter_currentAperture);
 		
 		ArrayAdapter<String> adapter_currentIso = new ArrayAdapter<String>(
 				this, android.R.layout.simple_spinner_item,
 				EvpairsCalculator.ISO_LIST);
 		adapter_currentIso.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_currentIso.setAdapter(adapter_currentIso);
+		mSpinner_currentIso.setAdapter(adapter_currentIso);
 		
 		ArrayAdapter<String> adapter_currentShutterSpeed = new ArrayAdapter<String>(
 				this, android.R.layout.simple_spinner_item,
 				EvpairsCalculator.SHUTTER_SPEED_LIST);
 		adapter_currentShutterSpeed.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_currentShutterSpeed.setAdapter(adapter_currentShutterSpeed);
+		mSpinner_currentShutterSpeed.setAdapter(adapter_currentShutterSpeed);
 
 		ArrayAdapter<String> adapter_newAperture = new ArrayAdapter<String>(
 				this, android.R.layout.simple_spinner_item,
 				EvpairsCalculator.APERTURE_LIST);
 		adapter_newAperture.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_newAperture.setAdapter(adapter_newAperture);
+		mSpinner_newAperture.setAdapter(adapter_newAperture);
 		
 		ArrayAdapter<String> adapter_newIso = new ArrayAdapter<String>(
 				this, android.R.layout.simple_spinner_item,
 				EvpairsCalculator.ISO_LIST);
 		adapter_newIso.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_newIso.setAdapter(adapter_newIso);
+		mSpinner_newIso.setAdapter(adapter_newIso);
 		
 		ArrayAdapter<String> adapter_newShutterSpeed = new ArrayAdapter<String>(
 				this, android.R.layout.simple_spinner_item,
 				EvpairsCalculator.SHUTTER_SPEED_LIST);
 		adapter_newShutterSpeed.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_newShutterSpeed.setAdapter(adapter_newShutterSpeed);		
+		mSpinner_newShutterSpeed.setAdapter(adapter_newShutterSpeed);
+				
 	}
+	
+	public void onClick(View v) {
+		getSelectedItemsPositions();
+		if (isRequiredFieldsFilled() == true) {
+			if (isOnlyOneFieldEmpty() == true) {
+				EvpairsCalculator evCalc = new EvpairsCalculator(
+						mCurrentAperturePosition, mCurrentIsoPosition,
+						mCurrentShutterSpeedPosition, mNewAperturePosition,
+						mNewIsoPostion, mNewShutterSpeedPosition);
+				int index = evCalc.calculate();
+
+				if (index != EvpairsCalculator.INVALID_INDEX) {
+					fillValueByIndex(index);
+				} else {
+					Toast.makeText(
+							this,
+							getString(R.string.evpairs_error_calculationProblem),
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		}
+	}
+
+	private void fillValueByIndex(int index) {
+		Spinner spinner;
+
+		if (mNewAperturePosition == 0) {
+			spinner = (Spinner) findViewById(R.id.evpairs_spinner_newAperture);
+		} else if (mNewIsoPostion == 0) {
+			spinner = (Spinner) findViewById(R.id.evpairs_spinner_newIso);
+		} else {
+			spinner = (Spinner) findViewById(R.id.evpairs_spinner_newShutterSpeed);
+		}
+
+		spinner.setSelection(index);
+	}
+	
+	/*
+	 * Verifies that all required fields are filled.
+	 * Function displays an error message if not all required fields have been filled.
+	 * 
+	 * @return true if all fields are filled or false in other case
+	 */
+	private boolean isRequiredFieldsFilled() {
+		boolean isFilled = false;
+
+		if (mCurrentAperturePosition == 0) {
+			Toast.makeText(this,
+					getString(R.string.evpairs_error_emptyCurrentAperture),
+					Toast.LENGTH_SHORT).show();
+		} else if (mCurrentIsoPosition == 0) {
+			Toast.makeText(this,
+					getString(R.string.evpairs_error_emptyCurrentIso),
+					Toast.LENGTH_SHORT).show();
+		} else if (mCurrentShutterSpeedPosition == 0) {
+			Toast.makeText(this,
+					getString(R.string.evpairs_error_emptyCurrentShutterSpeed),
+					Toast.LENGTH_SHORT).show();
+		} else {
+			isFilled = true;
+		}
+
+		return isFilled;
+	}
+	
+	/*
+	 * Verifies that only one new fields are empty.
+	 * Function display an error message if new fields does not contain only one empty field
+	 * 
+	 * @return true if new fields contain only one empty field
+	 */
+	private boolean isOnlyOneFieldEmpty() {
+		boolean isOnlyOneEmpty = false;
+		byte notEmptyCount = 0;
+
+		if (mNewAperturePosition > 0) {
+			notEmptyCount++;
+		}
+
+		if (mNewIsoPostion > 0) {
+			notEmptyCount++;
+		}
+
+		if (mNewShutterSpeedPosition > 0) {
+			notEmptyCount++;
+		}
+
+		if (notEmptyCount != MAXIMUM_ALLOWED_EMPTY_FIELDS) {
+			Toast.makeText(this,
+					getString(R.string.evpairs_error_onlyOneFieldMustBeEpmty),
+					Toast.LENGTH_SHORT).show();
+		} else {
+			isOnlyOneEmpty = true;
+		}
+
+		return isOnlyOneEmpty;
+	}
+	
+	/*
+	 * Function gets selected items positions for spinner
+	 */
+	private void getSelectedItemsPositions() {
+		mCurrentAperturePosition = mSpinner_currentAperture.getSelectedItemPosition();
+		mCurrentIsoPosition = mSpinner_currentIso.getSelectedItemPosition();
+		mCurrentShutterSpeedPosition = mSpinner_currentShutterSpeed.getSelectedItemPosition();
+		mNewAperturePosition = mSpinner_newAperture.getSelectedItemPosition();
+		mNewIsoPostion = mSpinner_newIso.getSelectedItemPosition();
+		mNewShutterSpeedPosition = mSpinner_newShutterSpeed.getSelectedItemPosition();		
+	}
+	
+	
+
 }
--- src/ru/neverdark/phototools/EvpairsCalculator.java
+++ src/ru/neverdark/phototools/EvpairsCalculator.java
@@ -1,21 +1,31 @@
 package ru.neverdark.phototools;
 
-public final class EvpairsCalculator {
+public class EvpairsCalculator {
 
-	static String ISO_LIST[] = { "", "25", "50", "100", "200", "400", "800",
+	private int mCurrentAperturePosition;
+	private int mCurrentIsoPosition;
+	private int mCurrentShutterSpeedPosition;
+	private int mNewAperturePosition;
+	private int mNewIsoPostion;
+	private int mNewShutterSpeedPosition;
+	private int mIndex;
+	
+	public static final int INVALID_INDEX = -1;
+	
+	public static final String ISO_LIST[] = { "", "25", "50", "100", "200", "400", "800",
 			"1600", "3200", "6400", "12800" };
 	
-	static String SHUTTER_SPEED_LIST[]  = { "", "512 min",
+	public static final String SHUTTER_SPEED_LIST[]  = { "", "512 min",
 		"256 min", "64 min", "32 min", "16 min", "8 min", "4 min", "2 min",
 		"1 min", "30 sec", "15 sec", "8 sec", "4 sec", "2 sec", "1 sec",
 		"1/2 sec", "1/4 sec", "1/8 sec", "1/15 sec", "1/30 sec",
 		"1/60 sec", "1/125 sec", "1/250 sec", "1/500 sec", "1/1000 sec",
 		"1/2000 sec", "1/4000 sec", "1/8000 sec" };
 		
-	static String APERTURE_LIST[] = { "", "1.4", "2.0", "2.8",
+	public static final String APERTURE_LIST[] = { "", "1.4", "2.0", "2.8",
 		"4", "5.6", "8", "11", "16", "22", "32" };
 	
-	static int EV_TABLE[][] = {
+	private final int EV_TABLE[][] = {
 		{ -16, -17, -18, -19, -20, -21, -22, -23, -24, -25 },
 		{ -15, -16, -17, -18, -19, -20, -21, -22, -23, -24 },
 		{ -14, -15, -16, -17, -18, -19, -20, -21, -22, -23 },
@@ -57,84 +67,86 @@ public final class EvpairsCalculator {
 		{ 22, 21, 20, 19, 18, 17, 16, 15, 14, 13 },
 		{ 23, 22, 21, 20, 19, 18, 17, 16, 15, 14 } };
 	
-	static String SHUTTERS_TABLE[][] = {
+	private final String SHUTTERS_TABLE[][] = {
 		{ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
 		{ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
 		{ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
 		{ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
 		{ "512 min", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
 		{ "256 min", "512 min", "0", "0", "0", "0", "0", "0", "0", "0" },
-		{ "128 min", "256 min", "512 min", "0", "0", "0", "0", "0", "0",
-				"0" },
-		{ "64 min", "128 min", "256 min", "512 min", "0", "0", "0", "0",
-				"0", "0" },
-		{ "32 min", "64 min", "128 min", "256 min", "512 min", "0", "0",
-				"0", "0", "0" },
-		{ "16 min", "32 min", "64 min", "128 min", "256 min", "512 min",
-				"0", "0", "0", "0" },
-		{ "8 min", "16 min", "32 min", "64 min", "128 min", "256 min",
-				"512 min", "0", "0", "0" },
-		{ "4 min", "8 min", "16 min", "32 min", "64 min", "128 min",
-				"256 min", "512 min", "0", "0" },
-		{ "2 min", "4 min", "8 min", "16 min", "32 min", "64 min",
-				"128 min", "256 min", "512 min", "0" },
-		{ "1 min", "2 min", "4 min", "8 min", "16 min", "32 min", "64 min",
-				"128 min", "256 min", "512 min" },
-		{ "30 sec", "1 min", "2 min", "4 min", "8 min", "16 min", "32 min",
-				"64 min", "128 min", "256 min" },
-		{ "15 sec", "30 sec", "1 min", "2 min", "4 min", "8 min", "16 min",
-				"32 min", "64 min", "128 min" },
-		{ "8 sec", "15 sec", "30 sec", "1 min", "2 min", "4 min", "8 min",
-				"16 min", "32 min", "64 min" },
-		{ "4 sec", "8 sec", "15 sec", "30 sec", "1 min", "2 min", "4 min",
-				"8 min", "16 min", "32 min" },
-		{ "2 sec", "4 sec", "8 sec", "15 sec", "30 sec", "1 min", "2 min",
-				"4 min", "8 min", "16 min" },
-		{ "1 sec", "2 sec", "4 sec", "8 sec", "15 sec", "30 sec", "1 min",
-				"2 min", "4 min", "8 min" },
-		{ "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec", "15 sec",
-				"30 sec", "1 min", "2 min", "4 min" },
-		{ "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec",
-				"15 sec", "30 sec", "1 min", "2 min" },
-		{ "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec",
-				"8 sec", "15 sec", "30 sec", "1 min" },
-		{ "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec",
-				"4 sec", "8 sec", "15 sec", "30 sec" },
-		{ "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec",
-				"2 sec", "4 sec", "8 sec", "15 sec" },
-		{ "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec",
-				"1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec" },
-		{ "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec",
-				"1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec" },
-		{ "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec",
-				"1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec" },
-		{ "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec",
-				"1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec" },
-		{ "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec",
-				"1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec" },
-		{ "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec",
-				"1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec",
-				"1/4 sec" },
-		{ "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec",
-				"1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec",
-				"1/15 sec", "1/8 sec" },
-		{ "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec",
-				"1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec",
-				"1/30 sec", "1/15 sec" },
-		{ "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec",
-				"1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec",
-				"1/30 sec" },
-		{ "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec",
-				"1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec" },
-		{ "0", "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec",
-				"1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec" },
-		{ "0", "0", "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec",
-				"1/1000 sec", "1/500 sec", "1/250 sec" },
-		{ "0", "0", "0", "0", "0", "1/8000 sec", "1/4000 sec",
-				"1/2000 sec", "1/1000 sec", "1/500 sec" },
-		{ "0", "0", "0", "0", "0", "0", "1/8000 sec", "1/4000 sec",
-				"1/2000 sec", "1/1000 sec" },
-		{ "0", "0", "0", "0", "0", "0", "0", "1/8000 sec", "1/4000 sec",
-				"1/2000 sec" } };
+		{ "128 min", "256 min", "512 min", "0", "0", "0", "0", "0", "0", "0" },
+		{ "64 min", "128 min", "256 min", "512 min", "0", "0", "0", "0", "0", "0" },
+		{ "32 min", "64 min", "128 min", "256 min", "512 min", "0", "0", "0", "0", "0" },
+		{ "16 min", "32 min", "64 min", "128 min", "256 min", "512 min", "0", "0", "0", "0" },
+		{ "8 min", "16 min", "32 min", "64 min", "128 min", "256 min", "512 min", "0", "0", "0" },
+		{ "4 min", "8 min", "16 min", "32 min", "64 min", "128 min", "256 min", "512 min", "0", "0" },
+		{ "2 min", "4 min", "8 min", "16 min", "32 min", "64 min", "128 min", "256 min", "512 min", "0" },
+		{ "1 min", "2 min", "4 min", "8 min", "16 min", "32 min", "64 min", "128 min", "256 min", "512 min" },
+		{ "30 sec", "1 min", "2 min", "4 min", "8 min", "16 min", "32 min", "64 min", "128 min", "256 min" },
+		{ "15 sec", "30 sec", "1 min", "2 min", "4 min", "8 min", "16 min", "32 min", "64 min", "128 min" },
+		{ "8 sec", "15 sec", "30 sec", "1 min", "2 min", "4 min", "8 min", "16 min", "32 min", "64 min" },
+		{ "4 sec", "8 sec", "15 sec", "30 sec", "1 min", "2 min", "4 min", "8 min", "16 min", "32 min" },
+		{ "2 sec", "4 sec", "8 sec", "15 sec", "30 sec", "1 min", "2 min", "4 min", "8 min", "16 min" },
+		{ "1 sec", "2 sec", "4 sec", "8 sec", "15 sec", "30 sec", "1 min", "2 min", "4 min", "8 min" },
+		{ "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec", "15 sec", "30 sec", "1 min", "2 min", "4 min" },
+		{ "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec", "15 sec", "30 sec", "1 min", "2 min" },
+		{ "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec", "15 sec", "30 sec", "1 min" },
+		{ "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec", "15 sec", "30 sec" },
+		{ "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec", "15 sec" },
+		{ "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec", "8 sec" },
+		{ "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec", "4 sec" },
+		{ "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec", "2 sec" },
+		{ "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec", "1 sec" },
+		{ "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec", "1/2 sec" },
+		{ "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec", "1/4 sec" },
+		{ "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec", "1/8 sec" },
+		{ "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec", "1/15 sec" },
+		{ "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec", "1/30 sec" },
+		{ "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec", "1/60 sec" },
+		{ "0", "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec", "1/125 sec" },
+		{ "0", "0", "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec", "1/250 sec" },
+		{ "0", "0", "0", "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec", "1/500 sec" },
+		{ "0", "0", "0", "0", "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec", "1/1000 sec" },
+		{ "0", "0", "0", "0", "0", "0", "0", "1/8000 sec", "1/4000 sec", "1/2000 sec" } };
 	
+	
+	public EvpairsCalculator(int currentAperturePosition,
+			int currentIsoPosition, int currentShutterSpeedPosition,
+			int newAperturePosition, int newIsoPostion,
+			int newShutterSpeedPosition) {
+
+		mCurrentAperturePosition = currentAperturePosition;
+		mCurrentIsoPosition = currentIsoPosition;
+		mCurrentShutterSpeedPosition = currentShutterSpeedPosition;
+		
+		mNewAperturePosition = newAperturePosition;
+		mNewIsoPostion = newIsoPostion;
+		mNewShutterSpeedPosition = newShutterSpeedPosition;
+		
+		mIndex = INVALID_INDEX;
+	}
+	
+	public int calculate() {				
+		if (mNewAperturePosition == 0) {
+			calculateAperture();
+		} else if (mNewIsoPostion == 0) {
+			calculateIso();
+		} else {
+			calculateShutterSpeed();
+		}
+		
+		return mIndex;
+	}
+	
+	private void calculateAperture() {
+		
+	}
+	
+	private void calculateShutterSpeed() {
+		
+	}
+	
+	private void calculateIso() {
+		
+	}
 }
