8128d8f501462b4673d08a1a0d7edd14f101ac72
Artem Yankovskiy <artemyankovskiy@gmail.com>
2014-02-23 22:33:11 +1100

    Finished plugins fragment

--- src/ru/neverdark/phototools/fragments/PluginsFragment.java
+++ src/ru/neverdark/phototools/fragments/PluginsFragment.java
@@ -1,6 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2014 Artem Yankovskiy (artemyankovskiy@gmail.com).
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package ru.neverdark.phototools.fragments;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 import ru.neverdark.phototools.R;
@@ -8,24 +23,46 @@ import ru.neverdark.phototools.utils.MainMenuItem;
 import ru.neverdark.phototools.utils.PluginAdapter;
 import ru.neverdark.phototools.utils.PluginManager;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.TabHost;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
-public class PluginsFragment extends SherlockFragment{
+public class PluginsFragment extends SherlockFragment {
+
+    private class AvailableItemClickListener implements OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                long id) {
+            MainMenuItem item = mAvailablePlugins.getItem(position);
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW);
+            marketIntent.setData(Uri.parse("market://details?id".concat(item
+                    .getPluginPackage())));
+            startActivity(marketIntent);
+        }
+    }
+
     private View mView;
     private ListView mAvailableListView;
     private ListView mInstalledListView;
     private Context mContext;
-    
-    
-    /* (non-Javadoc)
-     * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+    private PluginAdapter mAvailablePlugins;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater,
+     * android.view.ViewGroup, android.os.Bundle)
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,26 +70,28 @@ public class PluginsFragment extends SherlockFragment{
         super.onCreateView(inflater, container, savedInstanceState);
         mView = inflater.inflate(R.layout.activity_plugins, container, false);
         mContext = getSherlockActivity();
-        
+
         bindObjectsToResources();
-        
+
         buildTabs();
-        
+
+        mAvailableListView
+                .setOnItemClickListener(new AvailableItemClickListener());
         return mView;
     }
-    
+
     @Override
     public void onResume() {
         super.onResume();
         loadDataToLists();
     }
-    
+
     /**
      * Builds tabs
      */
     private void buildTabs() {
         TabHost mTabHost = (TabHost) mView.findViewById(android.R.id.tabhost);
-        
+
         mTabHost.setup();
 
         TabHost.TabSpec spec = mTabHost.newTabSpec("tag1");
@@ -66,21 +105,57 @@ public class PluginsFragment extends SherlockFragment{
         spec.setIndicator(getString(R.string.plugins_installed));
         mTabHost.addTab(spec);
     }
-    
+
     /**
      * Binds classes objects to resources
      */
     private void bindObjectsToResources() {
-        mAvailableListView = (ListView) mView.findViewById(R.id.plugins_listView_available);
-        mInstalledListView = (ListView) mView.findViewById(R.id.plugins_listView_installed);
+        mAvailableListView = (ListView) mView
+                .findViewById(R.id.plugins_listView_available);
+        mInstalledListView = (ListView) mView
+                .findViewById(R.id.plugins_listView_installed);
     }
-    
+
     /**
      * Loads data to lists
      */
     private void loadDataToLists() {
-        List<MainMenuItem> installed = PluginManager.getInstance(mContext).scan().getMenuItems();
-        PluginAdapter adapter = new PluginAdapter(mContext, R.layout.plugin_item, installed);
-        mInstalledListView.setAdapter(adapter);
+        List<MainMenuItem> installed = PluginManager.getInstance(mContext)
+                .scan().getMenuItems();
+        PluginAdapter installedPlugins = new PluginAdapter(mContext,
+                R.layout.plugin_installed_item, installed, true);
+        mInstalledListView.setAdapter(installedPlugins);
+
+        mAvailablePlugins = new PluginAdapter(mContext,
+                R.layout.plugin_available_item, buildAvailableList(), false);
+        mAvailableListView.setAdapter(mAvailablePlugins);
+    }
+
+    /**
+     * Builds list of available plug-ins
+     * 
+     * @return list of available plug-ins
+     */
+    private List<MainMenuItem> buildAvailableList() {
+        List<MainMenuItem> list = new ArrayList<MainMenuItem>();
+
+        list.add(createAvailableItem("ru.neverdark.phototools.azimuth",
+                "Azimuth plugin",
+                getResources().getDrawable(R.drawable.ic_plugin_azimuth)));
+
+        list.add(createAvailableItem("ru.neverdark.phototools.azimuthfree",
+                "Azimuth plugin (Free)",
+                getResources().getDrawable(R.drawable.ic_plugin_azimuth_free)));
+
+        return list;
+    }
+
+    private MainMenuItem createAvailableItem(String packageName,
+            String pluginName, Drawable pluginIcon) {
+        MainMenuItem item = new MainMenuItem();
+        item.setPluginPackage(packageName);
+        item.setAppName(pluginName);
+        item.setIcon(pluginIcon);
+        return item;
     }
 }
--- src/ru/neverdark/phototools/utils/PluginAdapter.java
+++ src/ru/neverdark/phototools/utils/PluginAdapter.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (C) 2014 Artem Yankovskiy (artemyankovskiy@gmail.com).
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package ru.neverdark.phototools.utils;
 
 import java.util.List;
@@ -16,48 +31,68 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
- * Array adapter for plugin
+ * Array adapter for plug-in
  */
 public class PluginAdapter extends ArrayAdapter<MainMenuItem> {
 
     private final List<MainMenuItem> mObjects;
     private final int mResource;
     private final Context mContext;
-    
+    private final boolean mIsInstalled;
+
     private static class RowHolder {
         private ImageView mPluginIcon;
         private TextView mPluginLabel;
         private ImageView mPluginRemove;
     }
-    
+
     /**
      * Constructor
-     * @param context application context
-     * @param resource resource id
-     * @param objects plug-in list
+     * 
+     * @param context
+     *            application context
+     * @param resource
+     *            resource id
+     * @param objects
+     *            plug-in list
+     * @param isInstalled
+     *            true for list installed plug-in
      */
     public PluginAdapter(Context context, int resource,
-            List<MainMenuItem> objects) {
+            List<MainMenuItem> objects, boolean isInstalled) {
         super(context, resource, objects);
         mContext = context;
         mResource = resource;
         mObjects = objects;
+        mIsInstalled = isInstalled;
     }
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         View row = convertView;
-        
+
         RowHolder holder = null;
-        
+
         if (row == null) {
             LayoutInflater inflater = (LayoutInflater) mContext
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             row = inflater.inflate(mResource, parent, false);
             holder = new RowHolder();
-            holder.mPluginIcon = (ImageView) row.findViewById(R.id.plugin_icon);
-            holder.mPluginLabel = (TextView) row.findViewById(R.id.plugin_label);
-            holder.mPluginRemove = (ImageView) row.findViewById(R.id.plugin_remove);
+
+            if (mIsInstalled) {
+                holder.mPluginIcon = (ImageView) row
+                        .findViewById(R.id.plugin_icon);
+                holder.mPluginLabel = (TextView) row
+                        .findViewById(R.id.plugin_label);
+                holder.mPluginRemove = (ImageView) row
+                        .findViewById(R.id.plugin_remove);
+            } else {
+                holder.mPluginIcon = (ImageView) row
+                        .findViewById(R.id.plugin_available_icon);
+                holder.mPluginLabel = (TextView) row
+                        .findViewById(R.id.plugin_available_label);
+            }
+
             row.setTag(holder);
         } else {
             holder = (RowHolder) row.getTag();
@@ -66,20 +101,24 @@ public class PluginAdapter extends ArrayAdapter<MainMenuItem> {
         MainMenuItem item = mObjects.get(position);
         holder.mPluginIcon.setImageDrawable(item.getIcon());
         holder.mPluginLabel.setText(item.getAppName());
-        
-        holder.mPluginRemove.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                removeApplication(mObjects.get(position).getPluginPackage());
-            }
-        });
-        
+
+        if (mIsInstalled) {
+            holder.mPluginRemove.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    removeApplication(mObjects.get(position).getPluginPackage());
+                }
+            });
+        }
+
         return row;
     }
 
     /**
      * Shows uninstall dialog
-     * @param pluginPackage package name for uninstall
+     * 
+     * @param pluginPackage
+     *            package name for uninstall
      */
     private void removeApplication(String pluginPackage) {
         Uri packageURI = Uri.parse("package:".concat(pluginPackage));
